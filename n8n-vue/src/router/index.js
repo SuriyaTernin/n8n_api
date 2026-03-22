eb2f8bc610e5c1bbe4dab8454ccc1ba19ed0251a
SuriyaTernin <diw0870238289z@gmail.com>
2026-03-22 16:30:53 +0700

    Initial commit

---
+++ n8n-vue/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/Register.vue'
+
+const routes = [
+  {
+    path: '/Register',
+    name: 'Register',
+    component: HomeView
+  },
+  {
+    path: '/Showdata',
+    name: 'Showdata',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Showdata.vue')
+  }
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
